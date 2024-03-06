b0bf4ed7f93632edd376e63270a52fb5404c2165
Skonhet <isa.jmartinez@hotmail.es>
2024-03-05 20:00:26 -0500

    First commit: Frontend:  HTML, css, js

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,25 @@
-console.log(tempData);
+const filmSection = document.getElementById('films')
+
+function renderFilms(movie){
+
+    const movieElement = document.createElement('article')
+    const card = document.createElement('div')
+
+    card.classList.add('divMovie')
+
+    card.innerHTML = `<img src="${movie.poster}" arc="${movie.title}">`
+
+    card.innerHTML = `
+    <h3>${movie.title} (${movie.year})</h3>
+    <p><strong>Director:</strong> ${movie.director}</p>
+    <p><strong>Duración:</strong> ${movie.duration}</p>
+    <p><strong>Género:</strong> ${movie.genre.join(', ')}</p>
+    <p><strong>Rate:</strong> ${movie.rate}</p>
+    `
+
+    filmSection.appendChild(movieElement)
+    movieElement.appendChild(containerMovie)
+}
+
+tempData.forEach(renderFilms);
+
