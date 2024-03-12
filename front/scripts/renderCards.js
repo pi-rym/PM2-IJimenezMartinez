a352aa291835c14d7acc0c95bed68a2adad68f0c
Skonhet <isa.jmartinez@hotmail.es>
2024-03-11 21:33:28 -0500

    Fifth commit: bundle

---
+++ front/scripts/renderCards.js
@@ -0,0 +1,27 @@
+const filmSection = document.getElementById('films')
+
+    function renderFilms(movie){
+
+        const movieElement = document.createElement('article')
+        const containerMovie = document.createElement('div')
+        movieElement.classList.add('movie')
+        containerMovie.classList.add('divMovie')
+    
+        movieElement.innerHTML = `<img src="${movie.poster}" arc="${movie.title}">`
+    
+        containerMovie.innerHTML = `
+        <h3>${movie.title} (${movie.year})</h3>
+        <p><strong>Director:</strong> ${movie.director}</p>
+        <p><strong>Duración:</strong> ${movie.duration}</p>
+        <p><strong>Género:</strong> ${movie.genre.join(', ')}</p>
+        <p><strong>Rate:</strong> ${movie.rate}</p>
+        `
+    
+        filmSection.appendChild(movieElement)
+        movieElement.appendChild(containerMovie)
+    }
+
+    $.get("https://students-api.up.railway.app/movies", (data) => data.forEach(renderFilms));
+
+
+module.exports = renderCards;
