6d1850827f66318dd619fcb6f7c30a5754eaee87
Skonhet <isa.jmartinez@hotmail.es>
2024-03-20 04:12:09 -0500

    testing

--- challenge-testing/index.js
+++ challenge-testing/index.js
@@ -1,3 +1,28 @@
-class carritoCompra {}
+class CarritoCompra {
+    constructor(){
+        this.products = []
+    }
 
-module.export = carritoCompra;
+    agregarProducto(product){
+        this.products.push(product)
+    }
+
+    calcularTotal(){
+        let total = 0;
+
+        for (const item of this.products) {
+           total += item.price * item.quantity
+        }
+
+        return total
+    }
+
+    aplicarDescuento(porcentaje){
+        const subtotal = this.calcularTotal()
+        const descuento = this.calcularTotal() * (porcentaje/100)
+
+        return subtotal - descuento
+    }
+}
+
+module.exports = CarritoCompra;
--- challenge-testing/test/carritoCompra.test.js
+++ challenge-testing/test/carritoCompra.test.js
@@ -1,1 +1,92 @@
-const carritoCompra = require("../index");
+const CarritoCompra = require("../index")
+
+/*
+    -CarritoCompra debería ser una clase
+    -carritoCompra debería ser una instancia de la clase CarritoCompra
+    -Debería guardar productos en una lista
+    -Deberia tener un método llamado agregarProducto
+    -Deberia tener un método llamado calcularTotal
+    -Deberia tener un método llamado aplicarDescuento
+    -El método agregarProduto debería agregar un producto a la lista
+    -El método calcularTotal debería sumar los precios de los productos del carrito
+    -El método aplicarDescuento debería aplicar un descuento al total de la compra
+*/
+
+describe('Clase CarritoCompra', ()=> {
+    let carritoCompra;
+
+    beforeEach (()=> {
+        carritoCompra = new CarritoCompra()
+    })
+
+    describe('Sobre el constructor de la clase CarritoCompra', ()=>{
+
+        it ('CarritoCompra debería ser una clase', ()=>{
+            expect(typeof CarritoCompra.prototype.constructor).toBe('function')
+        })
+
+        it('carritoCompra debería ser una instancia de la clase CarritoCompra', ()=> {
+            expect(carritoCompra instanceof CarritoCompra).toBe(true)
+        })
+
+        it('Debería guardar productos en una lista', ()=>{
+            expect(carritoCompra.products).toEqual([])
+        })
+    })
+
+    describe('Métodos de la clase CarritoCompra', ()=> {
+        const product1 = {
+            id:1,
+            name:'Agenda',
+            price: 30.00,
+            quantity:3
+        }
+
+        const product2 = {
+            id:2,
+            name:'Marcador',
+            price: 10.00,
+            quantity:6
+        }
+
+        it('Deberia tener un método llamado agregarProducto', ()=> {
+            expect(typeof carritoCompra.agregarProducto).toBe('function')
+        })
+
+        it('Deberia tener un método llamado calcularTotal', ()=> {
+            expect(typeof carritoCompra.calcularTotal).toBe('function')
+        })
+
+        it('Deberia tener un método llamado aplicarDescuento', ()=> {
+            expect(typeof carritoCompra.aplicarDescuento).toBe('function')
+        })
+
+        it('El método agregarProduto debería agregar un producto a la lista', ()=> {
+            carritoCompra.agregarProducto(product1)
+            expect(carritoCompra.products).toContain(product1)
+        })
+
+        it('El método calcularTotal debería sumar los precios de los productos del carrito', ()=> {
+            carritoCompra.agregarProducto(product1)
+            carritoCompra.agregarProducto(product2)
+
+            const totalExpected = product1.price * product1.quantity + product2.price * product2.quantity
+            
+            expect(carritoCompra.calcularTotal()).toBe(totalExpected)
+        })
+
+        it('El método aplicarDescuento debería aplicar un descuento al total de la compra', ()=> {
+            carritoCompra.agregarProducto(product1)
+            carritoCompra.agregarProducto(product2)
+
+            const subtotal = product1.price * product1.quantity + product2.price * product2.quantity
+
+            const descuento = 25
+
+            const totalExpected = subtotal - (subtotal * (descuento/100))
+            const total = carritoCompra.aplicarDescuento(descuento)
+
+            expect(total).toBe(totalExpected)
+        })
+    })
+    })
